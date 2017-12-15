eea1db27cf4e6d3fa532dbacfff611525da1b2c4
Yasser Mahmud <yasser.mahmud@gmail.com>
2017-12-15 20:54:48 +0500

    Initial commit

---
+++ src/ForagerBee.js
@@ -0,0 +1,17 @@
+import Bee from './Bee'
+
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+
+  }
+  forage() {
+    this.treasureChest.push('treasure');
+  }
+};
+
+export default ForagerBee;
